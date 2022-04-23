d7786c2d1a645abecf2edf34ec8b3815c81b8b99
Valeriy Sobolev <vsobolev93@list.ru>
2022-04-23 21:33:31 +0400

    добавлен метод

--- Задача_2/Program.cs
+++ Задача_2/Program.cs
@@ -1,5 +1,11 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 Console.WriteLine("Введите необходимое число");
+int Func(int x)
+{
+    int a = x / 10;
+    int b = a % 10;
+    return b;
+}
 int number = Convert.ToInt32(Console.ReadLine());
 if (number>99)
 {   
@@ -9,9 +15,8 @@ if (number>99)
     }
     else
     {
-        int a = number / 10;
-        int b = a % 10;
-        Console.WriteLine($"Вторая цифра числа = {b}");
+        int contr = Func(number);
+        Console.WriteLine($"Вторая цифра числа = {contr}");
     }
 }
 else
