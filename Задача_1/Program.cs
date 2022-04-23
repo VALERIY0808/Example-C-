c7ac59d02d8b2c3ec5def2eb4cd70ab34e299f06
Valeriy Sobolev <vsobolev93@list.ru>
2022-04-23 20:01:21 +0400

    проверка метода

--- Задача_1/Program.cs
+++ Задача_1/Program.cs
@@ -1,2 +1,11 @@
-﻿// Напишите метод, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-Console.WriteLine("Hello, World!");
+﻿// Напишите метод, который выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+int GetNumber (int value);{
+Console.WriteLine("Вводим случайного числа");
+int value = new Random().Next(10,100);
+int a = value / 10;
+int b = value % 10;
+// System.Console.WriteLine($"value = {value} a:{a} b:{b}");
+int max = a;
+ if (b>max) max=b;
+ return max;
+ }
