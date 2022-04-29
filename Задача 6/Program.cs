cc9c72342e2cec41545e42e92188bbd0f5becf90
Valeriy Sobolev <vsobolev93@list.ru>
2022-04-29 17:44:30 +0400

    задача 6 решена

---
+++ Задача 6/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход отрицательное четырехзначное число 
+// и на выходе показывает вторую цифру этого числа.
+int Method(int x)
+{
+    if (x<0)
+    {
+        if (x<-999)
+        {
+            if (x>-9999)
+            {
+                x = x/100;
+                x = x % 10;
+                x = -x;
+                return x;
+            }
+            else
+            {
+                Console.WriteLine($"Число {x} не подходит условию");
+                return x;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Число {x} не подходит условию");
+            return x;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Число {x} не подходит условию");
+        return x;
+    }
+    
+}
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int contr = Method(number);
+Console.WriteLine($"Вторая цифра числа {number} =  {contr}");
