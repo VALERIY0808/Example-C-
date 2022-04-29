72cdea5a457beac2dc121fc84f7f1c508bb607f2
Valeriy Sobolev <vsobolev93@list.ru>
2022-04-29 17:14:18 +0400

    задача 5 решена

---
+++ Задача 5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+int Method(int x,int y)
+{
+    if (y == x*x)
+    {
+        Console.WriteLine($"Число {y} является квадратом числа {x}");
+    }
+    else
+    {
+        Console.WriteLine($"Число {y}  не является квадратом числа {x}");
+    }
+    return y;
+}
+Console.WriteLine("Введите первое число");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+int contr = Method(firstnumber,secondnumber);
