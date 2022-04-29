56b59622399a91dcd5ff0f7c734418a11e91df02
Valeriy Sobolev <vsobolev93@list.ru>
2022-04-29 16:54:13 +0400

    задача 4 решена

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+int Method(int x)
+{
+    if (x % 7 ==0)
+    {
+        if (x % 23 == 0)
+        {
+            Console.WriteLine($"Число {x} кратно 23 и 7 одновременно");
+        }
+        else
+        {
+            Console.WriteLine($"Число {x}  не кратно 23 и 7 одновременно");
+        }
+    }   
+    else
+    {
+    Console.WriteLine($"Число {x}  не кратно 23 и 7 одновременно");
+    }
+    return x;
+}
+Console.WriteLine("Введите число для проверки");
+int number = Convert.ToInt32(Console.ReadLine());
+int contr = Method(number);
