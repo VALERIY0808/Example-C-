39cd5427eacda94783e056f643cd91ddd0879c30
Valeriy Sobolev <vsobolev93@list.ru>
2022-05-04 14:03:32 +0400

    задача 1 из 3 семинара  решена

---
+++ Семинар 3/Задача_1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+int Method(int x,int y)
+{
+    if (x==0 | y==0)
+    {
+        Console.WriteLine("Координата точки = 0 ");
+        return x;
+    } 
+    else
+    {
+        if (x>0)
+        {
+            if (y>0)
+            {
+                Console.WriteLine("Точка находится в 1 четверти");
+                return x;
+            }
+            else
+            {
+                Console.WriteLine("Точка находится в 4 четверти");
+                return x;
+            }
+
+        }
+        else
+        {
+            if (y>0)
+            {
+                Console.WriteLine("Точка находится в 2 четверти");
+                return x;
+            }
+            else
+            {
+                Console.WriteLine("Точка находится в 3 четверти");
+                return x;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Введите первую координату заданнной точки");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите вторую координату заданнной точки");
+int b = Convert.ToInt32(Console.ReadLine());
+int contr = Method(a,b);
